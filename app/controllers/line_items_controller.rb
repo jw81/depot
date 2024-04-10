ea8d9a7289e301b1360da65fc1cd305985daa802
Jeffrey Wallace <jeffrey.wallace81@gmail.com>
2024-04-10 17:47:33 -0500

    Integrates Hotwire into the cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -30,6 +30,8 @@ class LineItemsController < ApplicationController
       if @line_item.save
         # reset the store index visit per the challenge on pg.125
         session[:store_index_visits] = 0
+
+        format.turbo_stream
         format.html { redirect_to store_index_url }
         format.json { render :show, status: :created, location: @line_item }
       else
